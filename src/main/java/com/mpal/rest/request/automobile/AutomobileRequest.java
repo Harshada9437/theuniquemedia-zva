95b0f9280bff9a1a9e6e19839b3dc158f76233c3
Sumedh <sumedh1990@gmail.com>
2016-08-18 11:11:29 +0530

    Fix for automobile create- Code reformat

--- src/main/java/com/mpal/rest/request/automobile/AutomobileRequest.java
+++ src/main/java/com/mpal/rest/request/automobile/AutomobileRequest.java
@@ -5,52 +5,54 @@ package com.mpal.rest.request.automobile;
  */
 public class AutomobileRequest {
 
-        private String company;
-        private String model;
-        private String builtYear;
+    private String company;
+    private String model;
+    private String builtYear;
     private int automobileTypeId;
 
-        public String getCompany() { return  company; }
+    public String getCompany() {
+        return company;
+    }
 
-        public void setCompany(final String company)
-        {
-            this.company=company;
-        }
+    public void setCompany(final String company) {
+        this.company = company;
+    }
 
-        public String getModel() { return  model; }
+    public String getModel() {
+        return model;
+    }
 
-        public void setModel(final String model)
-        {
-            this.model=model;
-        }
+    public void setModel(final String model) {
+        this.model = model;
+    }
 
-        public String getBuiltYear() { return  builtYear; }
+    public String getBuiltYear() {
+        return builtYear;
+    }
 
-        public void setBuiltYear(final String builtYear)
-        {
-            this.builtYear=builtYear;
-        }
+    public void setBuiltYear(final String builtYear) {
+        this.builtYear = builtYear;
+    }
 
     public int getAutomobileTypeId() {
         return automobileTypeId;
     }
 
-    public void setAutomobileTypeId(final int automobileTypeId)
-        {
-            this.automobileTypeId = automobileTypeId;
-        }
-
-
-        @Override
-        public String toString() {
-            return "AutomobileRequest{" +
-                    "company=" + company + '\'' +
-                    "model=" + model + '\'' +
-                    ", builtYear=" + builtYear + '\'' +
-                    ", automobileTypeId=" + automobileTypeId +
-                    '}';
-        }
-
-
+    public void setAutomobileTypeId(final int automobileTypeId) {
+        this.automobileTypeId = automobileTypeId;
     }
+
+
+    @Override
+    public String toString() {
+        return "AutomobileRequest{" +
+                "company=" + company + '\'' +
+                "model=" + model + '\'' +
+                ", builtYear=" + builtYear + '\'' +
+                ", automobileTypeId=" + automobileTypeId +
+                '}';
+    }
+
+
+}
 
